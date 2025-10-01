ca3b5857c40e886a652a5a41eed379f1af0d0a66
Eduardo Vega Rodríguez <eduardovr1999@gmail.com>
2025-10-01 12:34:54 +0200

    chore(main): enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { AppConfig } from './config/interfaces/app-config.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.setGlobalPrefix('api');
   const configService = app.get<ConfigService<AppConfig, true>>(ConfigService);
   await app.listen(configService.get('port'));
