16703af227d3d426b6e20b955c4f180348fdfbd9
Eduardo Vega Rodríguez <eduardovr1999@gmail.com>
2025-10-01 12:38:01 +0200

    chore(main): enable global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 
@@ -8,6 +9,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
   const configService = app.get<ConfigService<AppConfig, true>>(ConfigService);
   await app.listen(configService.get('port'));
 }
